6b2154046a8e4709d86f6b740cc262db6b6a29c5
lanhuxx <1060173881@qq.com>
2020-11-01 13:35:57 +0800

    Create image_reader.py

---
+++ codes/draw_response_histroy/image_reader.py
@@ -0,0 +1,11 @@
+import cv2
+
+def ImageReader(file_name, picture_path, picture_format=".jpg", size=64):
+	picture_name = picture_path + file_name + picture_format 
+	picture = cv2.imread(picture_name, 1) 
+	picture_height = picture.shape[0]
+	picture_width = picture.shape[1]
+	picture_resize_t = cv2.resize(picture, (size, size))
+	picture_resize = picture_resize_t / 127.5 - 1.
+	# picture_resize = picture_resize_t / 128 - 0.996
+	return picture_resize, picture_height, picture_width
